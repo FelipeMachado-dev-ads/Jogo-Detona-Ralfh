5d7129ccb49256e7b274a1dfd797525107970a3a
Felipe Machado Dev <felipe.dev.ads@gmail.com>
2025-02-04 23:28:00 -0300

    atualização 1.0.2

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -5,22 +5,56 @@ const state = {
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#timeleft"),
         score: document.querySelector("#score"),
-       // sond: document.querySelector("")
+
     },
     values: {
         hitPosition: 0,
-        curretTime: 60,
+        currentTime: 60,
         result: 0,
     },
     //actions seta açoes que rodão por tas do codigo.
     actions: {
-        timerId: setInterval(randomSquare, 800),
-        countDownTimerId: setInterval(countDown, 1000)
+        timerId: setInterval(randomSquare, 900),
+        countDownTimerId: setInterval(countDown, 1000),
+
     },
 };
 
 
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
 
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+        resetGame();
+
+    }
+}
+function resetGame() {
+    state.values.currentTime = 60;
+    state.values.result = 0;
+    state.values.hitPosition = null;
+
+    state.view.timeLeft.textContent = state.values.currentTime;
+    state.view.score.textContent = state.values.result;
+
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+    state.actions.countDownTimerId = setInterval(countDown, 1000);
+    state.actions.timerId = setInterval(randomSquare, 900);
+    playSound("sond-gamer");
+}
+
+//Função de setar os audios criando uma variavel universal.
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.1; // Audio.volume = ;
+    audio.play();
+
+}
 
 //Função de randomizar a janela onde o Ralfh irá aparecer.
 function randomSquare() {
@@ -33,34 +67,23 @@ function randomSquare() {
     state.values.hitPosition = randomSquare.id;
 
 }
-//Função de setar os audios criando uma variavel universal.
-function playSound(audioName) {
-    let sond = new Audio('./src/audios/${audioName}.m4a')
-   // Audio.volume = ;
-    Audio.play();
-    Audio.pause()
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }
+        });
+    });
 }
-
-function countDown() {
-    state.values.curretTime --;
-    state.view.timeLeft.textContent = state.values.curretTime;
-    
-
-    if (state.values.curretTime <= 0) {
-        clearInterval(state.actions.countDownTimerId);
-        clearInterval(state.actions.timerId);
-                //alert("Game Over! O seu resultado foi: " + state.values.result);
-
-    }
-
-}
-
-
-
-
 
 function init() {
-    
+    addListenerHitBox();
+    playSound("sond-gamer");
 }
 init();
 
